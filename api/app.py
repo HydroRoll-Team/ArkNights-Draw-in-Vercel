860724f95ac1f3319fcbe1ed1818564c4fbeae83
简律纯 <i@jyunko.cn>
2023-07-12 22:11:36 +0800

    Update app.py

--- api/app.py
+++ api/app.py
@@ -4,25 +4,30 @@ from flask import Flask, send_file
 from PIL import Image
 from os.path import dirname, abspath, join
 import io
+from drawHandleArk import (
+    single_image_handle,
+    ten_image_handle,
+    hundred_image_handle,
+    ten_draw,
+    get_mongolia
+)
 
 app = Flask(__name__)
 dir = dirname(abspath(__file__))
 
 @app.route("/arknights/arknightsdraw", methods=['POST', 'GET'])
 def arknights():
-    return "sss"
-
+    img = ten_draw()
+    file_object = io.BytesIO()
+    img.save(file_object, 'PNG')
+    file_object.seek(0)
+    return send_file(file_object, mimetype='image/PNG')
 
 
 @app.route('/',methods=['POST', 'GET'])
 def arknights_draw():
-    # 从数组加载图片
-    # arr = np.array(raw_data)
-    # img = Image.fromarray(arr.astype('uint8'))
     img = Image.open(join(dir, '..', 'docs', 'main.png'))
-    # 在内存中创建图片对象
     file_object = io.BytesIO()
-    # write PNG in file-object
     img.save(file_object, 'png')
     file_object.seek(0)
     return send_file(file_object, mimetype='image/png')
