04b2d35770e3026c320f4163a8f7924a70724e6a
简律纯 <i@jyunko.cn>
2023-07-12 22:07:49 +0800

    Update update.py

--- api/update.py
+++ api/update.py
@@ -13,14 +13,7 @@ class TableData:
     pass
 
 
-class CharData:
-    name: str  # 名称
-    star: int  # 星级
-    limited: bool  # 限定
-    getWay: str  # 获得途径
-    pass
-
-
+# 更新核心函数
 class UpdateHandle:
 
     def __init__(self, data_path: str, conf_path: str):
@@ -43,24 +36,27 @@ class UpdateHandle:
     async def download_file(self, url: str, name: str, path: str) -> bool:
         r"""下载文件
 
-        :param url: 指定发送群号
+        :param url: 下载链接
         :param name: 文件名
         :param path: 储存目录
         :rtype bool
         """
         dir_path = self.data_path + path
         file_path = self.data_path + path + name
+
         if not os.path.exists(dir_path):
             os.makedirs(dir_path)
             return True
         if os.path.exists(file_path):
+            # print("文件"+name+"已存在")
             return True
         try:
             async with aiohttp.ClientSession(headers=self.headers) as session:
                 async with session.get(url, timeout=10) as response:
                     async with aiofiles.open(str(file_path), "wb") as f:
                         await f.write(await response.read())
-            print(f"下载文件{name}成功，url：{url}，储存目录：{path}")
+            # print(f"下载文件{name}成功")
+            # print(f"下载文件{name}成功，url：{url}，储存目录：{path}")
             return True
         except TimeoutError:
             print(f"下载文件{name} 超时，url：{url}")
@@ -72,7 +68,7 @@ class UpdateHandle:
     def request_data(self, url: str, cookie: list):
         pass
 
-
+    # 获取url链接内容
     async def get_url(self, url: str) -> str:
         result = ""
         retry = 5
@@ -88,12 +84,11 @@ class UpdateHandle:
                 await asyncio.sleep(1)
         return result
 
+    # 显示日志
     def log_print(self, message: str) -> bool:
         pass
 
-    def test(self):
-        loop = asyncio.get_event_loop()
-        loop.run_until_complete(self.get_info())
+    #
 
 
 if __name__ == "__main__":
